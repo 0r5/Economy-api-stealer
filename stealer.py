9a8c5c3c0baeb99d304db4bbf50db9745941c1b7
kf <65902496+0r5@users.noreply.github.com>
2022-08-12 00:44:53 +0300

    Create stealer.py

---
+++ stealer.py
@@ -0,0 +1,12 @@
+'''
+Roblox made an unfinished (I think) api that allows you to gift robux to an user
+This robux stealer probably won't work, but it's just an example of what someone can do with the API.
+This only works on Chromium and Firefox, because I'm too lazy to add support for other browsers. Sorry, guys
+
+https://economy.roblox.com/docs#!/Gifting/post_v2_gift_robux
+
+At the moment, seems like Roblox has removed the api docs, so I'll try making this from memory
+
+'''
+
+# I'm not going to write the code because fuck roblox, but what one could do is get the users cookie, extract the robux balance from the userinfo api and change the amount of robux to gift to that value. Obviously, you're going to gift it to yourself
